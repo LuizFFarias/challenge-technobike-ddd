1badd36785c5f184e447dab58652de795811f95e
rafaellabastos <rafa.bastos@yahoo.com.br>
2023-11-13 22:28:42 -0300

    Arrumando a identação

--- src/main/java/br/com/fiap/technobike/model/repository/ConnectionFactory.java
+++ src/main/java/br/com/fiap/technobike/model/repository/ConnectionFactory.java
@@ -28,17 +28,21 @@ public class ConnectionFactory {
 		this.pass = pass;
 		this.driver = driver;
 	}
+	
 	/***
 	 * Método que inicia o login com o banco de dados
 	 * @author Luiz Fillipe
 	 */
+	
 	public static ConnectionFactory getInstance() {
 		ConnectionFactory result = instance;
 		if (result != null) {
 			return result;
 		}
+	
 		Properties prop = new Properties();
 		FileInputStream file = null;
+		
 		try {
 			file = new FileInputStream("./src/main/resources/aplication.properties");
 			prop.load(file);
@@ -49,12 +53,12 @@ public class ConnectionFactory {
 			String driver = prop.getProperty("datasource.driver-class-name");
 			file.close();
 		
-		if(instance == null) {
+		if (instance == null) {
 			instance = new ConnectionFactory(url, user, pass, driver);
 		}
 		return instance;
 	}
-		catch(FileNotFoundException e){
+		catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 		}
 		catch(IOException e) {
@@ -67,47 +71,51 @@ public class ConnectionFactory {
 	 * Método que abre a conexão com o banco de dados e verifica erros de login e SQL.
 	 * @author Luiz Fillipe
 	 */
+	
 	public Connection getConexao() {
 		try {
-			if(this.conexao != null && !this.conexao.isClosed()) {
+			if (this.conexao != null && !this.conexao.isClosed()) {
 				return this.conexao;
 			}
-			if(this.getDriver() == null || this.getDriver().equals("")) {
+			if (this.getDriver() == null || this.getDriver().equals("")) {
 				System.out.println("Erro: nome da classe");
 				throw new RuntimeException("Erro: nome da classe");
 			}
-			if(this.getUrl() == null || this.getUrl().equals("")) {
+			if (this.getUrl() == null || this.getUrl().equals("")) {
 				System.out.println("Erro: Url de conexao");
 				throw new RuntimeException("Erro: Url de conexao");
 			}
-			if(this.getUser() == null || this.getUser().equals("")) {
+			if (this.getUser() == null || this.getUser().equals("")) {
 				System.out.println("Erro: Usuario incorreto");
 				throw new RuntimeException("Erro: Usuario incorreto");
 			}
 			Class.forName(this.getDriver());
 			this.conexao = DriverManager.getConnection(this.getUrl(), this.getUser(), this.getPass());		
 		}
-		catch(ClassNotFoundException e){
+		catch (ClassNotFoundException e){
 			System.out.println("Erro nome da classe: " + e.getMessage());
 			System.exit(1);
 		}
-		catch(SQLException e){
+		catch (SQLException e){
 			System.out.println("Erro no SQL: " + e.getMessage());
 			System.exit(1);
 		}
 		return conexao;
 	}
+	
 	private String getPass() {
 		return pass;
 	}
+	
 	private String getDriver() {
 		return driver;
 	}
+	
 	private String getUrl() {
 		return url;
 	}
+	
 	private String getUser() {
 		return user;
 	}
-	
 }
